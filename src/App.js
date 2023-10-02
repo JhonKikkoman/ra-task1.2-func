7d5c5aa71e6390ca756b42d4c0a0d89a5a540897
Piskun_Ivan <tw1st3r111@mail.ru>
2023-10-02 21:54:48 +0300

    task1

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         <div className='highlight-overlay'></div>
       </div>
       <div className="window">
-        <ShopItemFunc item={item} />
+        <ShopItemFunc render={item} />
       </div>
     </div>
   )
--- src/components/store/store-func.jsx
+++ src/components/store/store-func.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 
 class ShopItemFunc extends Component {
+    // eslint-disable-next-line no-useless-constructor
+    // constructor(props) {
+    //     super(props)
+    // }
     render() {
         return (
             <div class="main-content">
-                <h2>{this.item.brand}</h2>
-                <h1>{this.item.title}</h1>
-                <h3>{this.item.description}</h3>
+                <h2>{this.props.render.brand}</h2>
+                <h1>{this.props.render.title}</h1>
+                <h3>{this.props.render.description}</h3>
                 <div class="description">
-                    {this.item.descriptionFull}
+                    {this.props.render.descriptionFull}
                 </div>
                 <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
                 <div class="divider"></div>
                 <div class="purchase-info">
-                    <div class="price">{this.item.currency}{this.item.price}</div>
+                    <div class="price">{this.props.render.currency}{this.props.render.price}</div>
                     <button>Добавить в корзину</button>
                 </div>
             </div>
